324b024883a81fa88c535bf7a21b304104693a15
becory <13202145+becory@users.noreply.github.com>
2025-05-20 09:06:25 +1000

    [feat]: on changed clean result

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Form } from "../components/Form";
 import { CPUModel } from "../types/types";
@@ -6,14 +6,16 @@ import { setup } from "../utils/test";
 
 describe("Form Component Tests", () => {
   const mockOnSubmit = jest.fn();
+  const mockSetChanged = jest.fn();
 
   beforeEach(() => {
     mockOnSubmit.mockClear();
+    mockSetChanged.mockClear();
   });
 
   // test default value
   test("should initialize with default values", () => {
-    render(<Form onSubmit={mockOnSubmit} />);
+    render(<Form onSubmit={mockOnSubmit} setChanged={mockSetChanged} />);
 
     // check CPU default value: X86
     const cpuSelect = screen.getByLabelText(/CPU/i);
@@ -29,7 +31,9 @@ describe("Form Component Tests", () => {
   });
 
   test("should submit with memorySize=0 when memory input is empty", async () => {
-    const { user } = setup(<Form onSubmit={mockOnSubmit} />);
+    const { user } = setup(
+      <Form onSubmit={mockOnSubmit} setChanged={mockSetChanged} />
+    );
 
     const submitButton = screen.getByText("Submit");
     await user.click(submitButton);
@@ -39,30 +43,37 @@ describe("Form Component Tests", () => {
         memorySize: 0,
       })
     );
+    expect(mockSetChanged).toHaveBeenCalledTimes(1);
   });
 
   // test Form submit
   test("should call onSubmit with correct hardware configuration when submitted", async () => {
     // render form
-    const { user } = setup(<Form onSubmit={mockOnSubmit} />);
+    const { user } = setup(
+      <Form onSubmit={mockOnSubmit} setChanged={mockSetChanged} />
+    );
 
     // select CPU
     const cpuSelect = screen.getByLabelText(/CPU/i);
     await user.click(cpuSelect);
     const powerOption = screen.getByText("Power");
     await user.click(powerOption);
+    expect(mockSetChanged).toHaveBeenCalledTimes(1);
 
     // enter memory size (valid value)
     const memoryInput = screen.getByLabelText(/Memory Size/i);
     await user.type(memoryInput, "4096");
+    expect(mockSetChanged).toHaveBeenCalledTimes(5);
 
     // select GPU Accelerator option
     const gpuCheckbox = screen.getByLabelText(/GPU Accelerator Card/i);
     await user.click(gpuCheckbox);
+    expect(mockSetChanged).toHaveBeenCalledTimes(6);
 
     // click submit
     const submitButton = screen.getByText("Submit");
     await user.click(submitButton);
+    expect(mockSetChanged).toHaveBeenCalledTimes(7);
 
     // validate onSubmit
     expect(mockOnSubmit).toHaveBeenCalledTimes(1);
@@ -74,31 +85,36 @@ describe("Form Component Tests", () => {
   });
 
   test("should handle a complete user workflow with multiple changes", async () => {
-    const { user } = setup(<Form onSubmit={mockOnSubmit} />);
+    const { user } = setup(
+      <Form onSubmit={mockOnSubmit} setChanged={mockSetChanged} />
+    );
 
     // 1. select Power CPU
     const cpuSelect = screen.getByLabelText(/CPU/i);
     await user.click(cpuSelect);
     const powerOption = screen.getByText("Power");
     await user.click(powerOption);
+    expect(mockSetChanged).toHaveBeenCalledTimes(1);
 
     // 2. enter a invalid memory size to display error message.
     const memoryInput = screen.getByLabelText(/Memory Size/i);
-    fireEvent.change(memoryInput, { target: { value: "3000" } });
-    fireEvent.blur(memoryInput);
+    await user.type(memoryInput, "3000");
     await waitFor(async () => {
       expect(screen.getByText(/multiple of 1024MB/i)).toBeInTheDocument();
     });
+    expect(mockSetChanged).toHaveBeenCalledTimes(5);
 
     // change to valid memory size
     user.clear(memoryInput);
     await user.type(memoryInput, "4096");
+    expect(mockSetChanged).toHaveBeenCalledTimes(10);
 
     // 3. checked GPU, unchecked GPU and Checked it again
     const gpuCheckbox = screen.getByLabelText(/GPU Accelerator Card/i);
     await user.click(gpuCheckbox); // checked
     await user.click(gpuCheckbox); // unchecked
     await user.click(gpuCheckbox); // Checked
+    expect(mockSetChanged).toHaveBeenCalledTimes(13);
 
     // 4. submit form
     const submitButton = screen.getByText("Submit");
@@ -110,13 +126,17 @@ describe("Form Component Tests", () => {
       memorySize: 4096,
       gpuAccelerator: true,
     });
+    expect(mockSetChanged).toHaveBeenCalledTimes(14);
   });
 
   test("should handle formatted memory input correctly", async () => {
-    const { user } = setup(<Form onSubmit={mockOnSubmit} />);
+    const { user } = setup(
+      <Form onSubmit={mockOnSubmit} setChanged={mockSetChanged} />
+    );
 
     const memoryInput = screen.getByLabelText(/Memory Size/i);
     await user.type(memoryInput, "524,288");
+    expect(mockSetChanged).toHaveBeenCalledTimes(7);
 
     // submit
     const submitButton = screen.getByText("Submit");
@@ -128,5 +148,6 @@ describe("Form Component Tests", () => {
         memorySize: 524288,
       })
     );
+    expect(mockSetChanged).toHaveBeenCalledTimes(8);
   });
 });
